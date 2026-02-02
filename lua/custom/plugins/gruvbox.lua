578d1d5c87947ba2d9bc8247856a88a8a4cd5a03
dalton hill <hill.dalton@heb.com>
2026-02-02 17:28:54 -0600

    moving gruvbox to init

--- lua/custom/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'sainnhe/gruvbox-material',
-  priority = 0,
-  config = function()
-    vim.o.background = 'dark'
-    vim.cmd [[ let g:gruvbox_material_background = 'hard' ]]
-    vim.cmd.colorscheme 'gruvbox-material'
-  end,
-}
