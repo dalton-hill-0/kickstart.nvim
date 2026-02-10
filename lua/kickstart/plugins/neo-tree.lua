47154189620aeb0a49c602812cbf4bf83b636e57
Dalton Hill <dalton.hill.0@protonmail.com>
2026-02-10 06:26:05 -0600

    neotree config for working and scratch dirs

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,17 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = true,
+        hide_gitignored = true,
+        hide_by_name = {},
+        never_show = {},
+        never_show_by_pattern = {},
+        -- Always show these directories even if gitignored
+        always_show = { 'working', 'scratch' },
+        always_show_by_pattern = { '*/working/*', '*/scratch/*' },
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
