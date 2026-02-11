c762a802bc32ce8e2bf2f39575947ef9d29dc662
dalton hill <hill.dalton@heb.com>
2026-02-11 17:37:25 -0600

    commenting out change to header, hard to see structure

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -4,9 +4,9 @@
 
 ;; Conceal ATX heading markers and following whitespace
 ;; Using offset! to extend the capture range to include the trailing space
-((atx_h1_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
-((atx_h2_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
-((atx_h3_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
-((atx_h4_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
-((atx_h5_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
-((atx_h6_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+; ((atx_h1_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+; ((atx_h2_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+; ((atx_h3_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+; ((atx_h4_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+; ((atx_h5_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+; ((atx_h6_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
