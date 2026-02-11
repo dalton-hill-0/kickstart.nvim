fa7b1c36f655bda0a7dc2bffa0933f2a803a5e5d
dalton hill <hill.dalton@heb.com>
2026-02-11 17:34:06 -0600

    concealing the trailing space of the headers

--- queries/markdown/highlights.scm
+++ queries/markdown/highlights.scm
@@ -3,11 +3,10 @@
 ((fenced_code_block_delimiter) @conceal (#set! conceal ""))
 
 ;; Conceal ATX heading markers and following whitespace
-;; By matching both the marker and space in sequence, we can conceal them together
-; ((atx_h1_marker) @conceal (#trim! @conceal 1 1 1 1) (#set! conceal ""))
-((atx_h1_marker) @conceal (#set! conceal ""))
-((atx_h2_marker) @conceal (#set! conceal ""))
-((atx_h3_marker) @conceal (#set! conceal ""))
-((atx_h4_marker) @conceal (#set! conceal ""))
-((atx_h5_marker) @conceal (#set! conceal ""))
-((atx_h6_marker) @conceal (#set! conceal ""))
+;; Using offset! to extend the capture range to include the trailing space
+((atx_h1_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+((atx_h2_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+((atx_h3_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+((atx_h4_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+((atx_h5_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
+((atx_h6_marker) @conceal (#offset! @conceal 0 0 0 1) (#set! conceal ""))
